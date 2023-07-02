8b567e3adb51c31e8941733b5be58dee3176cf07
Guy Ronen <guy.ronen@codility.com>
2023-07-02 17:29:43 +0300

    added todo for update and delete methods

--- backend/products/views.py
+++ backend/products/views.py
@@ -13,6 +13,7 @@ from .models import Product
 
 # This is a way to do it with class based views
 
+
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProcuctSerializer
@@ -29,17 +30,17 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProcuctSerializer
 
 
+# TODO: Add update and delete views
+
+
 ############### CONSTANTS FROM url.py ######################################################################
 product_list_create_api_view = ProductListCreateAPIView.as_view()
 product_detail_api_view = ProductDetailAPIView.as_view()
 ############################################################################################################
 
 
-
-
-
-
 # This is a way to do it with function based views
+
 
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
@@ -66,5 +67,4 @@ def product_alt_view(request, pk=None, *args, **kwargs):
             serializer.save()
             return Response(serializer.data, status=201)
         else:
-
             return Response({}, status=400)
