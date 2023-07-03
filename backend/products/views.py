e9c7a99f25a9ecc20da5224a74849e8e1f130464
Guy Ronen <guy.ronen@codility.com>
2023-07-03 10:18:38 +0300

    product mixins options

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 # DRF
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, mixins
 
 # Local
 from .serializers import ProcuctSerializer
@@ -47,6 +47,29 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
 
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
+
+
+
+## Example to Product api view using mixins
+
+# class ProductMxinAPIView(
+#     mixins.CreateModelMixin,
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     generics.GenericAPIView,
+# ):
+#     queryset = Product.objects.all()
+#     serializer_class = ProcuctSerializer
+#     lookup_field = "pk"
+
+#     def get(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk")
+#         if pk is not None:
+#             return self.retrieve(request, *args, **kwargs)
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs
 
 
 ############### CONSTANTS FROM url.py ######################################################################
